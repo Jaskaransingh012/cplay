5e3bcc48a3f84d91b0e86919bcaaa74a579ea328
jaskaransingh012 <jaskaransingh_1@outlook.com>
2026-09-21 17:13:18 +0530

    [Tested] updated the mp3 and its working fine

--- include/mp3.h
+++ include/mp3.h
@@ -8,4 +8,7 @@ int mp3_open(Mp3File **mp3, const char *path);
 size_t mp3_read(Mp3File *mp3, void *buffer, size_t buffer_size);
 void mp3_close(Mp3File *mp3);
 
+int mp3_get_sample_rate(Mp3File *mp3);
+int mp3_get_channels(Mp3File * mp3);
+
 #endif
